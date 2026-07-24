46055495887a372642337909a8664f37814d9199
Kavitha <kavithachirukuri378@gmail.com>
2026-07-24 13:40:27 +0530

    Add Decisions script and basic file

---
+++ Decisions_if_elif_else.py
@@ -0,0 +1,9 @@
+featuere_count = 500
+if featuere_count > 1000:
+    print("this is a large dataset.")
+elif featuere_count >100:
+    print("this is a medium dataset.")
+else:
+    print("this is a small dataset.")
+    
+
