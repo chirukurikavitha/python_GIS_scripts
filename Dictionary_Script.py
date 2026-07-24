711e2a89bf5c8f447b2f7151efe82b9767bb3665
Kavitha <kavithachirukuri378@gmail.com>
2026-07-24 13:14:30 +0530

    Add dictionary script and basic files

---
+++ Dictionary_Script.py
@@ -0,0 +1,13 @@
+layer_info = {
+"name" : "Parcles",
+"geometry_type" : "Polygon",
+"feature_count" : 1420
+}
+
+print(layer_info["name"])
+print(layer_info["geometry_type"])
+print(layer_info["feature_count"])
+print(layer_info.keys())
+print(layer_info.values())
+print(layer_info.items())
+
